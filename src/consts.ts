24cd2d34f2061306418870d357989a0c9865d1c9
FireIsGood <fireisadequate@gmail.com>
2024-05-01 17:31:14 -0700

    Add structured data for SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
 export const SITE_TITLE = "FireIsWebsite";
 export const SITE_DESCRIPTION = "The website of FireIsGood";
 export const SITE_EMBED_IMAGE = "embed-image.png";
+
+export const AUTHOR_NAME = "FireIsGood";
+export const AUTHOR_DESCRIPTION = "Programmer and blog author";
+export const AUTHOR_URL = "https://fireis.dev/about";
