2bee489047831e7994bef5fff47d5684160150d2
FireIsGood <fireisadequate@gmail.com>
2024-12-06 23:55:16 -0800

    feat: hide drafts from RSS
    oops

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,7 +6,7 @@ import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("blog", ({ data }) => (import.meta.env.PROD ? data.draft !== true : true));
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
